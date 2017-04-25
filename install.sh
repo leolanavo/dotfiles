14e15985ac40d7e99e0c747e78404f637f3c585f
leolanavo <leo0198@gmail.com>
2017-04-25 09:48:53 -0300

    Add the tmux.conf to the repository and installing script

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -sf inputrc ~/.inputrc
 ln -sf gitconfig ~/.gitconfig
 ln -sf bashrc ~/.bashrc
 ln -sf vim ~/.vim
+ln -sf tmux.conf ~/.tmux.conf
 
 git clone https://github.com/ryanoasis/nerd-fonts
 git clone https://github.com/powerline/fonts
