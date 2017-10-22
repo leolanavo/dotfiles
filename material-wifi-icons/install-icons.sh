4c3f220f1f53a77bc6918e2403d89356400d0d20
Leonardo Lana <leolanavo@gmail.com>
2017-10-22 12:32:07 -0200

    Add the ethernet icon and change png directory to wifi

--- material-wifi-icons/install-icons.sh
+++ material-wifi-icons/install-icons.sh
@@ -1,13 +1,20 @@
 #!/bin/zsh
 
-NMDIR=/usr/share/icons/hicolor/22x22/apps
-MTDIR=$(pwd)/png
+# Wifi icons
+NM_WIFI_DIR=/usr/share/icons/hicolor/22x22/apps
+MT_WIFI_DIR=$(pwd)/wifi
+
+# Ethernet icon
+NM_WIRE_DIR=/usr/share/icons/hicolor/16x16/apps
+MT_WIRE_DIR=$(pwd)
+
 IFS=$'\n'
-icons=($(ls $MTDIR))
-nm_icons=($(ls $NMDIR | grep nm-signal))
+icons=($(ls $MT_WIFI_DIR))
+nm_icons=($(ls $NM_WIFI_DIR | grep nm-signal))
 unset IFS
 
 for i in $(seq 1 5); do
-    cp -f $MTDIR/${icons[$i]} $NMDIR/${nm_icons[$i]}
+    cp -f $MT_WIFI_DIR/${icons[$i]} $NM_WIFI_DIR/${nm_icons[$i]}
 done
 
+cp -f $MT_WIRE_DIR/ethernet.png $NM_WIRE_DIR/nm-device-wired.png
