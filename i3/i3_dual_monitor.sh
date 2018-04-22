d7b99b7765cc3d44b5d08ea506f8433de6bc0849
Leonardo Lana <leolanavo@gmail.com>
2018-04-22 09:55:40 -0300

    Upgrade script for toggling between mirror and dual monitors
    Signed-off-by: Leonardo Lana <leolanavo@gmail.com>

--- i3/i3_dual_monitor.sh
+++ i3/i3_dual_monitor.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
- xrandr | grep 'HDMI1 connected' &&
-    xrandr --output eDP1 --auto --output HDMI1 --auto --left-of eDP1
+INTERAL_OUTPUT="eDP1"
+EXTERNAL_OUTPUT="HDMI1"
 
+if [ ! -f "/tmp/monitor_mode.dat" ] ; then
+  MONITOR_MODE="mirror"
+else
+  MONITOR_MODE=`cat /tmp/monitor_mode.dat`
+fi
+
+if [ $MONITOR_MODE == "mirror" ]; then
+    MONITOR_MODE="dual"
+    xrandr --output $INTERAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --left-of $INTERAL_OUTPUT
+else
+    MONITOR_MODE="mirror"
+    xrandr --output $INTERAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --same-as $INTERAL_OUTPUT
+fi
+
+echo "${MONITOR_MODE}" > /tmp/monitor_mode.dat
