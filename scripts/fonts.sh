a77b48279ad03dcf7c1b59b80e4ba70fcaca7b40
Leonardo Lana <leolanavo@gmail.com>
2018-10-18 08:32:24 -0300

    Migrate from aurman to yay
    Signed-off-by: Leonardo Lana <leolanavo@gmail.com>

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aurman -S nerd-fonts-complete ttf-font-awesome --noconfirm
+yay -S nerd-fonts-complete ttf-font-awesome --noconfirm
 git clone https://github.com/powerline/fonts.git --depth=1
 ./fonts/install.sh
 rm -rf fonts
