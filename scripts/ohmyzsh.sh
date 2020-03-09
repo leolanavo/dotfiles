8d3ea78c02cf1514622dd679cf4ee9df39a93e63
Leonardo Lana <leolanavo@gmail.com>
2020-03-09 09:12:08 -0300

    Update zshrc with powerline10k

--- scripts/ohmyzsh.sh
+++ scripts/ohmyzsh.sh
@@ -3,4 +3,5 @@
 rm -rf ~/.oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
