6bf07be5861ab64789e60043247be4ca90688e52
Leonardo Lana <leolanavo@gmail.com>
2020-10-10 20:27:01 -0300

    Update ohmyzsh script

--- scripts/ohmyzsh.sh
+++ scripts/ohmyzsh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf ~/.oh-my-zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
