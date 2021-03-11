04546b2ab2def069dd42e5381108015a96737d31
Leonardo Lana <leolanavo@gmail.com>
2021-03-11 11:07:56 -0300

    Improve nvim/lsp-config

--- nvim/lua/lsp-config.lua
+++ nvim/lua/lsp-config.lua
@@ -17,45 +17,34 @@ vim.lsp.handlers['textDocument/documentSymbol'] =
     require'lsputil.symbols'.document_handler
 vim.lsp.handlers['workspace/symbol'] =
     require'lsputil.symbols'.workspace_handler
-vim.lsp.handlers["textDocument/publishDiagnostics"] =
+vim.lsp.handlers['textDocument/publishDiagnostics'] =
     vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics,
                  {underline = true, virtual_text = false})
 
 local on_attach = function(client, bufnr)
-    local function buf_set_keymap(...)
-        vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
-
-    -- Mappings.
+  local function set_keymap(type, keymap, value)
     local opts = {noremap = true, silent = true}
-    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'gh', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>',
-                   opts)
-    buf_set_keymap('n', '<space>rm', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', '<space>rr', '<cmd>lua vim.lsp.buf.references()<CR>',
-                   opts)
-    buf_set_keymap('n', '<space>d',
-                   '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
-                   opts)
+    vim.api.nvim_buf_set_keymap(bufnr, type, keymap, value, opts)
+  end
 
-    buf_set_keymap('n', '<space>i', '<cmd>lua vim.lsp.buf.code_action()<CR>',
-                   opts)
-    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>',
-                   opts)
-    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>',
-                   opts)
+  -- Mappings.
+  set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>')
+  set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>')
+  set_keymap('n', 'gh', '<Cmd>lua vim.lsp.buf.hover()<CR>')
+  set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+  set_keymap('n', '<space>rm', '<cmd>lua vim.lsp.buf.rename()<CR>')
+  set_keymap('n', '<space>rr', '<cmd>lua vim.lsp.buf.references()<CR>')
+  set_keymap('n', '<space>d', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>')
+  set_keymap('n', '<space>i', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+  set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
+  set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
 
-    -- Set some keybinds conditional on server capabilities
-    if client.resolved_capabilities.document_formatting then
-        buf_set_keymap("n", "<space>=", "<cmd>lua vim.lsp.buf.formatting()<CR>",
-                       opts)
-    elseif client.resolved_capabilities.document_range_formatting then
-        buf_set_keymap("n", "<space>=", "<cmd>lua vim.lsp.buf.formatting()<CR>",
-                       opts)
-    end
+  -- Set some keybinds conditional on server capabilities
+  if client.resolved_capabilities.document_formatting then
+    set_keymap("n", "<space>=", "<cmd>lua vim.lsp.buf.formatting()<CR>")
+  elseif client.resolved_capabilities.document_range_formatting then
+    set_keymap("n", "<space>=", "<cmd>lua vim.lsp.buf.formatting()<CR>")
+  end
 end
 
 lspconfig.tsserver.setup {
