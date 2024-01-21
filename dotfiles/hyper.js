dc95d1c6f3f3797804243a57dee134394d271361
Leonardo Lana <leolanavo@gmail.com>
2024-01-21 17:32:40 -0300

    Updata zshrc powerlevel10k

--- dotfiles/hyper.js
+++ dotfiles/hyper.js
@@ -56,10 +56,10 @@ module.exports = {
         // an array here instead of a color map object
         colors: {
             black: '#000',
-            red: '#C51E14',
+            red: '#d04b43',
             green: '#1DC121',
             yellow: '#C7C329',
-            blue: '#0A2FC4',
+            blue: '#758add',
             magenta: '#C839C5',
             cyan: '#20C5C6',
             white: '#C7C7C7',
@@ -137,7 +137,13 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: ['hyper-font-ligatures', "hypercwd", "hyper-search", "hyper-pane"],
+    plugins: [
+        'hyper-font-ligatures',
+        "hypercwd",
+        "hyper-search",
+        "hyper-pane",
+        "hyper-tabs-enhanced"
+    ],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
@@ -146,5 +152,10 @@ module.exports = {
     // Example
     // 'window:devtools': 'cmd+alt+o',
     },
+    hyperTabs: {
+      activityColor: 'salmon',
+			border: true,
+    }
+
 };
 //# sourceMappingURL=config-default.js.map
