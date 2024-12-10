2853df9bc1350a553a5f42af4fbbfd48e95b0ac7
Leonardo Lana <leolanavo@gmail.com>
2024-12-10 11:02:03 -0300

    Add tmux config

--- dotfiles/hyper.js
+++ dotfiles/hyper.js
@@ -10,7 +10,7 @@ module.exports = {
         // default font size in pixels for all tabs
         fontSize: 14,
         // font family with optional fallbacks
-        fontFamily: '"FiraCode Nerd Font", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+        fontFamily: '"FiraCode Nerd Font Mono", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
         // default font weight: 'normal' or 'bold'
         fontWeight: 'normal',
         // font weight for bold characters: 'normal' or 'bold'
